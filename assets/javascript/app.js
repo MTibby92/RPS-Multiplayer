036ad57316cfb283311f80d614eeee78dd014d87
MTibby92 <MTibby92@gmail.com>
2016-09-07 18:40:42 -0500

    the sign in now works such that the database updates properly for player1 and player2 present; added the onclick functionality for rock paper scissors, but t here's a bug with the second selection

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,24 @@
 //global variables
 var database = firebase.database()
-// database.set({
-// 	playerList: [],
+database.ref().set({
+	player1Present: false,
+	player2Present: false,
+	player1ID: null,
+	player2ID: null,
+	player1Selection: null,
+	player2Selection: null,
+	player1Wins: 0,
+	player1Ties: 0,
+	player1Losses: 0,
+	player2Wins: 0,
+	player2Ties: 0,
+	player2Losses: 0
+})
 
-// })
+var player1Boolean = false
+var player2Boolean = false
+
+// var playerSelectionLocal = undefined
 
 function initApp() {
     // Listening for auth state changes.
@@ -11,8 +26,13 @@ function initApp() {
     firebase.auth().onAuthStateChanged(function(user) {
         if (user) {
             // User is signed in.
+            console.log('a user has signed in')
             var isAnonymous = user.isAnonymous
             var uid = user.uid
+            user.updateProfile({
+				displayName: "Test User"
+			})
+
             // [START_EXCLUDE]
             document.getElementById('quickstart-sign-in-status').textContent = 'Signed in';
             $('#signIn').html('Sign out')
@@ -40,6 +60,7 @@ function toggleSignIn() {
 		firebase.auth().signOut();
 		// [END signout]
 	} else {
+		$('.input').prop('disabled', false)
 		// [START authanon]
 		firebase.auth().signInAnonymously().catch(function(error) {
 			// Handle Errors here.
@@ -111,20 +132,94 @@ var game = {
 
 			}
 		}
-	},
-	getInput: function(event) {
-		var input = $(event.target).val()
-	},
+	}
+	// getInput: function(event) {
+	// 	var input = $(event.target).val()
+	// 	console.log(input)
+	// },
 }
 
+
+
+// At the initial load, this runs everytime the database updates
+firebase.database().ref().on('value', function(snapshot) {
+	console.log('In database check, player1Present is ' + snapshot.child('player1Present').val())
+	console.log('In database check, player2Present is ' + snapshot.child('player2Present').val())
+
+	player1Boolean = snapshot.child('player1Present').val()
+	player2Boolean = snapshot.child('player2Present').val()
+
+	if (snapshot.child("player1Present").val() == true && snapshot.child("player2Present").val() == true) {
+		console.log('Game ready to be played!')
+		$('.input').prop('disabled', false)
+
+		$('.input').on('click', function(event) {
+			var input = $(event.target).html()
+			console.log(input)
+
+			if (!snapshot.child('player1Selection').exists()) {
+				console.log('Updating player1Selection to input because it has no value yet')
+				database.ref().update({
+					"player1Selection": input
+				})
+			} else if (snapshot.child('player1Selection').exists() && !snapshot.child('player2Selection').exists()) {
+				console.log('Updating player2Selection to input because it has no value yet but selection 1 does')
+				database.ref().update({
+					"player2Selection": input
+				})
+			} else {
+				console.log('Both players have made their selection and stored it in the database')
+			}
+		})
+
+	} else if (snapshot.child("player1Present").val() == true && snapshot.child('player2Present').val() == false) {
+		console.log('Waiting on player 2')
+	} else {
+		console.log('Waiting on 2 players to join')
+	}
+})
+
+
+firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+			console.log('Player1Boolean is ' + player1Boolean)
+			console.log('Player2Boolean is ' + player2Boolean)
+
+            // User is signed in.
+            var isAnonymous = user.isAnonymous
+            var uid = user.uid
+            if (player1Boolean && player2Boolean) {
+            	console.log('Sorry, already two players! (Condition 1)')
+            } else if (player1Boolean && !player2Boolean) {
+            	console.log('Condition 2 met')
+            	database.ref().update({
+            		"player2Present": true
+            	})
+            } else if (!player1Boolean && !player2Boolean) {
+            	console.log('Condition 3 met')
+            	database.ref().update({
+            		"player1Present": true
+            	})
+            } else {
+            	console.log('Somehow none of the proper conditions were met')
+            }
+		} else {
+			console.log('user is not signed in')
+		}
+})
 
 window.onload = function() {
     initApp()
  }
 
 $(document).ready(function() {
-	$('.input').on('click', game.getInput(event))
-	$('#signIn').on('click', function(event) {
+	$('.input').prop('disabled', true)
+	// $('.input').on('click', function(event) {
+	// 	var input = $(event.target).html()
+	// 	console.log(input)
+	// 	input = playerSelectionLocal
 
-	})  
-})
+
+	// })
+	// $('#signIn').on('click', function(event) {})  
+});
